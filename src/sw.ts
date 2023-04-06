63f646b862cd5b8cf9fbc41107b54c582279353c
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2023-04-06 22:42:13 +0700

    feat: pwa sample

---
+++ src/sw.ts
@@ -0,0 +1,11 @@
+/// <reference lib="webworker" />
+
+import { precacheAndRoute } from 'workbox-precaching'
+
+declare let self: ServiceWorkerGlobalScope
+
+self.addEventListener('message', (event: any) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
+})
+
+precacheAndRoute(self.__WB_MANIFEST)
